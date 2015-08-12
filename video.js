913697192b3b286af63cdb863821c5b80c26a01b
Sri Rama Krishna Vardhan D <vardhan.d@ninestars.in>
2015-08-12 18:21:16 +0530

    Initial commit

---
+++ video.js
@@ -0,0 +1,31 @@
+var vid = document.getElementById("testid")
+var pauseButton = document.querySelector("#polina button");
+//console.log(document)
+alert(vid);
+alert(pauseButton);
+
+/*function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+vid.addEventListener('ended', function()
+{
+// only functional if "loop" is removed 
+vid.pause();
+// to capture IE10
+vidFade();
+}); 
+
+
+pauseButton.addEventListener("click", function() {
+	alert("hi");
+  vid.classList.toggle("stopfade");
+  if (vid.paused) {
+    vid.play();
+    pauseButton.innerHTML = "Pause";
+  } else {
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+  }
+})*/
+
